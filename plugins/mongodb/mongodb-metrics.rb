738184b96113c2468db616c6b0bbb4c6c57dc4fd
Coto Cisternas <cotocisternas@gmail.com>
2016-05-14 19:10:36 -0300

    update mongodb check and metrics

--- plugins/mongodb/mongodb-metrics.rb
+++ plugins/mongodb/mongodb-metrics.rb
@@ -1,6 +1,6 @@
 #! /usr/bin/env ruby
 #
-#   mongodb-metrics
+#   metrics-mongodb.rb
 #
 # DESCRIPTION:
 #
@@ -12,14 +12,15 @@
 #
 # DEPENDENCIES:
 #   gem: sensu-plugin
-#   gem: mongo 2.X
+#   gem: mongo
+#   gem: bson
+#   gem: bson_ext
 #
 # USAGE:
 #   #YELLOW
 #
 # NOTES:
 #   Basics from github.com/mantree/mongodb-graphite-metrics
-#   mongo driver must be 2.X not 1.X
 #
 # LICENSE:
 #   Copyright 2013 github.com/foomatty
@@ -27,10 +28,13 @@
 #   for details.
 #
 
-require 'rubygems' if RUBY_VERSION < '1.9.0'
 require 'sensu-plugin/metric/cli'
 require 'mongo'
 include Mongo
+
+#
+# Mongodb
+#
 
 class MongoDB < Sensu::Plugin::Metric::CLI::Graphite
   option :host,
@@ -59,82 +63,103 @@ class MongoDB < Sensu::Plugin::Metric::CLI::Graphite
          short: '-s SCHEME',
          default: "#{Socket.gethostname}.mongodb"
 
-  option :ssl,
-         description: 'Use SSL to connect',
-         long: '--ssl',
+  option :password,
+         description: 'MongoDB password',
+         long: '--password PASSWORD',
+         default: nil
+
+  option :debug,
+         description: 'Enable debug',
+         long: '--debug',
          default: false
 
-  option :ssl_cert,
-         description: 'SSL certificate (optional)',
-         long: '--ssl_cert /path/to/cert',
-         default: nil
-
-  option :ssl_ca_cert,
-         description: 'SSL CA certificate (optional)',
-         long: '--ssl_ca_cert /path/to/ca_cert',
-         default: nil
-
-  def get_mongo_doc(db, command)
+  def get_mongo_doc(command)
     rs = @db.command(command)
-    if rs.successful?
-      return rs.documents[0]
+    unless rs.successful?
+      return nil
+    end
+    rs.documents[0]
+  end
+
+  # connects to mongo and sets @db, works with MongoClient < 2.0.0
+  def connect_mongo_db(host, port, db_name, db_user, db_password)
+    if Gem.loaded_specs['mongo'].version < Gem::Version.new('2.0.0')
+      mongo_client = MongoClient.new(host, port)
+      @db = mongo_client.db(db_name)
+      @db.authenticate(db_user, db_password) unless db_user.nil?
+    else
+      address_str = "#{host}:#{port}"
+      client_opts = {}
+      client_opts[:database] = db_name
+      unless db_user.nil?
+        client_opts[:user] = db_user
+        client_opts[:password] = db_password
+      end
+      mongo_client = Mongo::Client.new([address_str], client_opts)
+      @db = mongo_client.database
     end
   end
 
   def run
-    address_str = "#{config[:host]}:#{config[:port]}"
-
-    client_opts = {}
-    client_opts[:database] = 'admin'
-    unless config[:user].nil?
-      client_opts[:user] = config[:user]
-      client_opts[:password] = config[:password]
+    Mongo::Logger.logger.level = Logger::FATAL
+    @debug = config[:debug]
+    if @debug
+      Mongo::Logger.logger.level = Logger::DEBUG
+      config_debug = config.clone
+      config_debug[:password] = '***'
+      puts 'arguments:' + config_debug.inspect
     end
-    client_opts[:ssl] = config[:ssl]
+    host = config[:host]
+    port = config[:port]
+    db_name = 'admin'
+    db_user = config[:user]
+    db_password = config[:password]
 
-    if client_opts[:ssl]
-      if config[:ssl_cert]
-        client_opts[:ssl_cert] = config[:ssl_cert]
-      end
-
-      if config[:ssl_ca_cert]
-        client_opts[:ssl_ca_cert] = config[:ssl_ca_cert]
-      end
-    end
-
-    mongo_client = Mongo::Client.new([address_str], client_opts)
-    @db = mongo_client.database
+    connect_mongo_db(host, port, db_name, db_user, db_password)
 
     _result = false
     # check if master
     begin
-      is_master = get_mongo_doc(@db, 'isMaster' => 1)
-      unless is_master.nil?
-        _result = is_master['ok'] == 1
+      @is_master = get_mongo_doc('isMaster' => 1)
+      unless @is_master.nil?
+        _result = @is_master['ok'] == 1
       end
-    rescue
+    rescue StandardError => e
+      if @debug
+        puts 'Error checking isMaster:' + e.message
+        puts e.backtrace.inspect
+      end
       exit(1)
     end
+
     # get the metrics
     begin
       metrics = {}
-      server_status = get_mongo_doc(@db, 'serverStatus' => 1)
-      unless server_status.nil? || server_status['ok'] != 1
+      server_status = get_mongo_doc('serverStatus' => 1)
+      if !server_status.nil? && server_status['ok'] == 1
         metrics.update(gather_replication_metrics(server_status))
         timestamp = Time.now.to_i
         metrics.each do |k, v|
           output [config[:scheme], k].join('.'), v, timestamp
         end
       end
-      ok
-    rescue
-      exit(1)
+    rescue StandardError => e
+      if @debug
+        puts 'Error checking serverStatus:' + e.message
+        puts e.backtrace.inspect
+      end
+      exit(2)
     end
+
+    # done!
+    ok
   end
 
   def gather_replication_metrics(server_status)
+    mongo_version = server_status['version'].gsub(/[^0-9\.]/i, '') # Handle versions like "2.6.11-pre" etc
     server_metrics = {}
-    server_metrics['lock.ratio'] = "#{sprintf('%.5f', server_status['globalLock']['ratio'])}" unless server_status['globalLock']['ratio'].nil?
+
+    server_metrics['lock.ratio'] = sprintf('%.5f', server_status['globalLock']['ratio']).to_s unless server_status['globalLock']['ratio'].nil?
 
     server_metrics['lock.queue.total'] = server_status['globalLock']['currentQueue']['total']
     server_metrics['lock.queue.readers'] = server_status['globalLock']['currentQueue']['readers']
@@ -143,18 +168,20 @@ class MongoDB < Sensu::Plugin::Metric::CLI::Graphite
     server_metrics['connections.current'] = server_status['connections']['current']
     server_metrics['connections.available'] = server_status['connections']['available']
 
-    if server_status['indexCounters']['btree'].nil?
-      server_metrics['indexes.missRatio'] = "#{sprintf('%.5f', server_status['indexCounters']['missRatio'])}"
-      server_metrics['indexes.hits'] = server_status['indexCounters']['hits']
-      server_metrics['indexes.misses'] = server_status['indexCounters']['misses']
-    else
-      server_metrics['indexes.missRatio'] = "#{sprintf('%.5f', server_status['indexCounters']['btree']['missRatio'])}"
-      server_metrics['indexes.hits'] = server_status['indexCounters']['btree']['hits']
-      server_metrics['indexes.misses'] = server_status['indexCounters']['btree']['misses']
+    if Gem::Version.new(mongo_version) < Gem::Version.new('3.0.0')
+      if server_status['indexCounters']['btree'].nil?
+        server_metrics['indexes.missRatio'] = sprintf('%.5f', server_status['indexCounters']['missRatio']).to_s
+        server_metrics['indexes.hits'] = server_status['indexCounters']['hits']
+        server_metrics['indexes.misses'] = server_status['indexCounters']['misses']
+      else
+        server_metrics['indexes.missRatio'] = sprintf('%.5f', server_status['indexCounters']['btree']['missRatio']).to_s
+        server_metrics['indexes.hits'] = server_status['indexCounters']['btree']['hits']
+        server_metrics['indexes.misses'] = server_status['indexCounters']['btree']['misses']
+      end
     end
 
-    server_metrics['cursors.open'] = server_status['cursors']['totalOpen']
-    server_metrics['cursors.timedOut'] = server_status['cursors']['timedOut']
+    server_metrics['cursors.open'] = server_status['metrics']['cursor']['open']['total']
+    server_metrics['cursors.timedOut'] = server_status['metrics']['cursor']['timedOut']
 
     server_metrics['mem.residentMb'] = server_status['mem']['resident']
     server_metrics['mem.virtualMb'] = server_status['mem']['virtual']
